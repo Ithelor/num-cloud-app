b475ae4b8ab566a17c2a4e2e7d9fb8766315e84d
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-01-13 14:37:04 +1000

    Redundant functionality removed, overall fixes

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ require('dotenv').config({
 })
 
 import TelegramBot, { SendMessageOptions } from 'node-telegram-bot-api'
-import mongoose, { Mongoose } from 'mongoose'
+import mongoose from 'mongoose'
 
 import {
   getCodeByName,
@@ -16,7 +16,6 @@ import {
 import { uploadObject, deleteObject, waitUntilExists } from './util/Storage'
 import speechToText from './util/SpeechToText'
 
-import ChatSchema from './models/Chat'
 import UserSchema from './models/User'
 
 const MONGO_URI = process.env.MONGO_URI!
@@ -38,60 +37,16 @@ try {
 const token = process.env.API_KEY
 const bot = new TelegramBot(token!, { polling: true })
 
-// Handling /start
-bot.onText(/\/start/, async (msg: TelegramBot.Message) => {
-  const params = { reply_to_message_id: msg.message_id }
-  const Chat = new ChatSchema({ id: msg.chat.id, contactedAt: Date.now() })
-
-  ChatSchema.findOne({ id: msg.chat.id }, (err: any, data: any) => {
-    if (err) {
-      console.log(`An error occured: ${err}`)
-    } else {
-      if (data) {
-        bot.sendMessage(
-          msg.chat.id,
-          `This chat has already been initialized - @${msg.from!.username}`,
-          params
-        )
-      } else {
-        Chat.save((err: never) => {
-          if (err) {
-            return console.log(`An error occured while saving a model: ${err}`)
-          }
-        })
-        bot.sendMessage(msg.chat.id, `Initialized succesfully`, params)
-      }
-    }
-  })
-})
-
-// Handling /t -text-
-bot.onText(/\/t\s(.+)|\/t@CloudElevenBot\s(.+)/, (msg: any, match: any) => {
-  const params = {
-    reply_to_message_id: msg.message_id
-  }
-  const resp: string = match?.[1] || match?.[2]
-
-  ChatSchema.findOne({ id: msg.chat.id }).then(
-    (data: { settings: { stl: string } }) => {
-      translateText(resp, data.settings.stl || 'en').then((res) => {
-        return bot.sendMessage(msg.chat.id, res, params)
-      })
-    }
-  )
-})
-
-// Handling /tl -language- -text-
-// TODO: fix no reply
+// /tl -language- -text-
 bot.onText(
   /\/tl\s([A-Za-z]+)\s(.+)|\/tl@CloudElevenBot\s([A-Za-z]+)\s(.+)/,
-  async (msg: any, match: any) => {
+  async (msg: TelegramBot.Message, match: RegExpExecArray | null) => {
     const params = { reply_to_message_id: msg.message_id }
-    let lang: string = match?.[1] || match?.[3]
-    let resp: string = match?.[2] || match?.[4]
+    let lang = match?.[1] || match?.[3]
+    let resp = match?.[2] || match?.[4]
 
-    const gotNameByCode = await getNameByCode(lang),
-      gotCodeByName = await getCodeByName(lang)
+    const gotNameByCode = await getNameByCode(lang!),
+      gotCodeByName = await getCodeByName(lang!)
 
     Promise.all([gotNameByCode, gotCodeByName]).then((data) => {
       if (!data[0] && !data[1]) {
@@ -104,115 +59,128 @@ bot.onText(
 
       lang = data[1] ??= lang
 
-      translateText(resp, lang).then((res) => {
-        bot.sendMessage(msg.chat.id, res)
+      translateText(resp!, lang!).then((res) => {
+        bot.sendMessage(msg.chat.id, res, params)
       })
     })
   }
 )
 
-// Handling /stl -lang-
-bot.onText(/\/stl (.+)/, async (msg: any, match: any) => {
-  const params = {
-    reply_to_message_id: msg.message_id,
-    parse_mode: 'HTML' as TelegramBot.ParseMode
-  }
-  let lang: string = match?.[1]!
-  let langName: string = match?.[1]!
-
-  let gotNameByCode = await getNameByCode(lang),
-    gotCodeByName = await getCodeByName(lang)
-
-  Promise.all([gotNameByCode, gotCodeByName]).then((data) => {
-    if (!data[0] && !data[1]) {
-      return bot.sendMessage(
-        msg.chat.id,
-        `Language not recognized. Please try again`,
-        params
-      )
+// Alternative /tl -lang-
+bot.onText(
+  /\/tl ([A-Za-z]+)$|\/tl@CloudElevenBot ([A-Za-z]+)$/,
+  async (msg: TelegramBot.Message, match: RegExpExecArray | null) => {
+    let params: SendMessageOptions = {
+      reply_to_message_id: msg.message_id
     }
 
-    lang = gotCodeByName ??= lang
-    langName = gotNameByCode ??= langName.charAt(0).toUpperCase() + langName.slice(1)
+    let resp: string
+    let lang: string = match?.[1]!
 
-    ChatSchema.findOne(
-      { id: msg.chat.id },
-      (err: any, res: { settings: { stl: string } }) => {
-        if (lang.localeCompare(res.settings.stl) === 0) {
-          return bot.sendMessage(
-            msg.chat.id,
-            `<b><i>${langName}</i></b> already set as a target language`,
-            params
-          )
-        }
-        ChatSchema.updateOne(
-          { id: msg.chat.id },
-          { settings: { stl: lang } },
-          { upsert: true }
+    let gotNameByCode = await getNameByCode(lang),
+      gotCodeByName = await getCodeByName(lang)
+
+    Promise.all([gotNameByCode, gotCodeByName]).then(async (data) => {
+      if (!data[0] && !data[1]) {
+        return bot.sendMessage(
+          msg.chat.id,
+          `Language not recognized. Please try again`,
+          params
         )
-          .then(() => {
-            return bot.sendMessage(
-              msg.chat.id,
-              `<b><i>${langName}</i></b> set as a target language`,
-              params
-            )
-          })
-          .catch((err: any) => {
-            console.log(`An error occured while updating a model: ${err}`)
-          })
       }
-    )
-  })
-})
 
-//	Handling /status
-bot.onText(/\/status/, async (msg: any, match: any) => {
-  const params = {
-    reply_to_message_id: msg.message_id
-  }
+      lang = gotCodeByName ??= lang
 
-  ChatSchema.findOne(
-    { id: msg.chat.id },
-    (err: any, res: { settings: { stl: string; spc: boolean } }) => {
-      let mes: string
-      getNameByCode(res.settings.stl).then((data) => {
-        if (!err) mes = `Current target language: ${data}\nauto: ${res.settings.spc}`
-        else mes = `Unable to fetch chat settings`
+      if (msg.reply_to_message) {
+        params = {
+          ...params,
+          reply_to_message_id: msg.reply_to_message!.message_id
+        }
+        // Handling voice messages
+        if (msg.reply_to_message.voice) {
+          UserSchema.findOne(
+            { id: msg!.reply_to_message!.from!.id },
+            async (err: string, data: { until: Date }) => {
+              if (data) {
+                if (data.until > new Date()) {
+                  await bot
+                    .getFileLink(msg.reply_to_message!.voice!.file_id)
+                    .then(async (url) => {
+                      // TEMPORAL upload to Google Cloud Storage
+                      // literally like for 5 seconds 🙄
+                      const path = `audio-files/${String(msg.from!.id)}/${
+                        msg.reply_to_message!.voice!.file_id
+                      }.ogg`
+                      uploadObject(url, path).then(() => {
+                        bot
+                          .sendMessage(
+                            msg.chat.id,
+                            `Audio uploaded, running transcription`,
+                            {
+                              reply_to_message_id: msg.reply_to_message!.message_id
+                            }
+                          )
+                          .then((msgData) => {
+                            waitUntilExists(path).then(() => {
+                              speechToText(path).then((stt_data) => {
+                                bot.editMessageText(
+                                  `Audio transcripted, running translation`,
+                                  {
+                                    chat_id: msgData.chat.id,
+                                    message_id: msgData.message_id
+                                  }
+                                )
+                                translateText(String(stt_data), lang).then((res) => {
+                                  return bot.editMessageText(res, {
+                                    chat_id: msgData.chat.id,
+                                    message_id: msgData.message_id
+                                  })
+                                })
+                                deleteObject(path)
+                              })
+                            })
+                          })
+                      })
+                    })
+                } else {
+                  bot.sendMessage(
+                    msg.chat.id,
+                    `Your subscription has expired.\nIf you consider renewing it, see /subscribe`,
+                    {
+                      reply_to_message_id: msg.message_id
+                    }
+                  )
+                }
+              } else {
+                bot.sendMessage(
+                  msg.chat.id,
+                  `Voice messages translation is available via paid subscription.\nSee /subscribe`,
+                  {
+                    reply_to_message_id: msg.message_id
+                  }
+                )
+              }
+            }
+          )
+        } else {
+          resp = msg.reply_to_message.text!
 
-        bot.sendMessage(msg.chat.id, mes, params)
-      })
-    }
-  )
-})
-
-//	Handling /t
-bot.onText(/\/t$|\/t@CloudElevenBot$/, async (msg: any) => {
-  const params = {
-    reply_to_message_id: msg.message_id
-  }
-  let resp: string, lang: string, opts: SendMessageOptions
-
-  if (msg.reply_to_message) {
-    ChatSchema.findOne({ id: msg.chat.id }).then(
-      (data: { settings: { stl: string } }) => {
-        lang = data.settings.stl
-        resp = msg.reply_to_message.text
-
-        translateText(resp, lang || 'en').then((res) => {
-          return bot.sendMessage(msg.chat.id, (res ??= ''), params)
-        })
+          translateText(resp, lang!).then((res) => {
+            return bot.sendMessage(msg.chat.id, res, params)
+          })
+        }
+      } else {
+        bot.sendMessage(
+          msg.chat.id,
+          `Please specify text or reply to a message`,
+          params
+        )
       }
-    )
-  } else {
-    return bot.sendMessage(
-      msg.chat.id,
-      `Please specify text or reply to a message`,
-      params
-    )
+    })
   }
-})
+)
 
-// Handling /subscribe
+// /subscribe
 bot.onText(/\/subscribe/, async (msg: TelegramBot.Message) => {
   const params = {
     reply_to_message_id: msg.message_id,
@@ -237,14 +205,12 @@ bot.onText(/\/subscribe/, async (msg: TelegramBot.Message) => {
   )
 })
 
-// Handling callback queries
+// Callbacks
 bot.on('callback_query', (callbackQuery) => {
   if (callbackQuery.data === 'sub') {
     const msg = callbackQuery.message
 
     bot.answerCallbackQuery(callbackQuery.id).then(() => {
-      console.log(msg!)
-
       const expires = new Date(
         new Date().getFullYear(),
         new Date().getMonth() + 1,
@@ -254,11 +220,13 @@ bot.on('callback_query', (callbackQuery) => {
 
       UserSchema.findOne(
         { id: msg!.reply_to_message!.from!.id },
-        (err: any, data: any) => {
+        (err: string, data: { until: Date }) => {
           if (data) {
-            newExpires = new Date(
-              data.until.setMonth(data.until.getMonth() + 1)
-            ).toDateString()
+            if (data.until > new Date()) {
+              newExpires = new Date(
+                data.until.setMonth(data.until.getMonth() + 1)
+              ).toDateString()
+            }
           }
           UserSchema.findOneAndUpdate(
             { id: msg!.reply_to_message!.from!.id },
@@ -288,144 +256,8 @@ bot.on('callback_query', (callbackQuery) => {
   }
 })
 
-/*
-  Handling alternative / incorrect queries
-*/
-
-// Handling /tl
-bot.onText(/\/tl$|\/tl@CloudElevenBot$/, async (msg: any) => {
-  const params = {
-    reply_to_message_id: msg.message_id
-  }
-
-  bot.sendMessage(msg.chat.id, `Please specify target language and text`, params)
-})
-
-// Handling /tl -lang-
-bot.onText(
-  /\/tl ([A-Za-z]+)$|\/tl@CloudElevenBot ([A-Za-z]+)$/,
-  async (msg: any, match: any) => {
-    let params: SendMessageOptions = {
-      reply_to_message_id: msg.message_id
-    }
-
-    let resp: string
-
-    let lang: string = match?.[1]!
-    let langName: string = match?.[1]!
-
-    let gotNameByCode = await getNameByCode(lang),
-      gotCodeByName = await getCodeByName(lang)
-
-    Promise.all([gotNameByCode, gotCodeByName]).then(async (data) => {
-      if (!data[0] && !data[1]) {
-        return bot.sendMessage(
-          msg.chat.id,
-          `Language not recognized. Please try again`,
-          params
-        )
-      }
-
-      lang = gotCodeByName ??= lang
-      langName = gotNameByCode ??=
-        langName.charAt(0).toUpperCase() + langName.slice(1)
-
-      if (msg.reply_to_message) {
-        params = {
-          ...params,
-          reply_to_message_id: msg.reply_to_message!.message_id
-        }
-        if (msg.reply_to_message.voice) {
-          UserSchema.findOne(
-            { id: msg!.reply_to_message!.from!.id },
-            async (err: any, data: any) => {
-              if (data) {
-                if (data.until > new Date().getDate()) {
-                  console.log(
-                    `data.until (${
-                      data.until
-                    }) > new Date().getDate() ${new Date().getDate()}`
-                  )
-                  await bot
-                    .getFileLink(msg.reply_to_message.voice!.file_id)
-                    .then(async (url) => {
-                      // TEMPORAL upload to Google Cloud Storage
-                      // seriously like for 5 seconds 🙄
-                      const path = `audio-files/${String(msg.from!.id)}/${
-                        msg.reply_to_message.voice!.file_id
-                      }.ogg`
-                      uploadObject(url, path).then(() => {
-                        bot
-                          .sendMessage(
-                            msg.chat.id,
-                            `Audio uploaded, running transcription`,
-                            {
-                              reply_to_message_id: msg.reply_to_message.message_id
-                            }
-                          )
-                          .then((msgData) => {
-                            waitUntilExists(path).then(() => {
-                              speechToText(path).then((stt_data) => {
-                                bot.editMessageText(
-                                  `Audio transcripted, running translation`,
-                                  {
-                                    chat_id: msgData.chat.id,
-                                    message_id: msgData.message_id
-                                  }
-                                )
-                                translateText(String(stt_data), lang).then((res) => {
-                                  // return bot.sendMessage(msg.chat.id, res, params)
-                                  return bot.editMessageText(res, {
-                                    chat_id: msgData.chat.id,
-                                    message_id: msgData.message_id
-                                  })
-                                })
-                                deleteObject(path)
-                              })
-                            })
-                          })
-                      })
-                    })
-                }
-              } else {
-                bot.sendMessage(
-                  msg.chat.id,
-                  `Voice messages translation is available via paid subscription.\nSee /subscribe`,
-                  {
-                    reply_to_message_id: msg.message_id
-                  }
-                )
-              }
-            }
-          )
-        } else {
-          resp = msg.reply_to_message.text
-
-          translateText(resp, lang!).then((res) => {
-            return bot.sendMessage(msg.chat.id, res, params)
-          })
-        }
-      } else {
-        bot.sendMessage(
-          msg.chat.id,
-          `Please specify text or reply to a message`,
-          params
-        )
-      }
-    })
-  }
-)
-
-// Handling /stl
-bot.onText(/\/stl$|\/stl@CloudElevenBot$/, async (msg: any) => {
-  const params = {
-    reply_to_message_id: msg.message_id
-  }
-  bot.sendMessage(msg.chat.id, `Please specify target language`, params)
-})
-
-// Handling inlines
-bot.on('inline_query', async (msg) => {
+// Inlines
+bot.on('inline_query', async (msg: TelegramBot.InlineQuery) => {
   if (msg.query) {
     let [, nope, lang, text] = msg.query.match(/(\w+)$|(\w+)\s(.+)/) || []
     let title: string, description: string, message_text: string
@@ -482,7 +314,7 @@ bot.on('inline_query', async (msg) => {
           translateText(text, langCode).then((data) => {
             title ??= `Translation (${langName}): ${data}`
             description ??= `Original (${detectedLanguageName}): ${text}`
-            message_text = `${title}\n${description}`
+            message_text = data
 
             bot.answerInlineQuery(msg.id, [
               {
@@ -500,4 +332,17 @@ bot.on('inline_query', async (msg) => {
         })
     }
   }
+})
+
+// Incorrect /tl
+bot.onText(/\/tl$|\/tl@CloudElevenBot$/, async (msg: TelegramBot.Message) => {
+  const params = {
+    reply_to_message_id: msg.message_id
+  }
+
+  bot.sendMessage(
+    msg.chat.id,
+    `Incorrect query.\nPlease either follow "language text" syntax or reply to a message with language specified`,
+    params
+  )
 })
